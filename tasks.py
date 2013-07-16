5ee29c5ad24f3ef04e4b116221a19275aaf4c137
Noriyuki Hosaka <bgnori@gmail.com>
2013-07-16 16:57:21 +0900

    fetch and mark 	modified:   tasks.py

--- tasks.py
+++ tasks.py
@@ -14,12 +14,24 @@ celery = Celery('tasks', broker='amqp://guest@localhost//')
 
 celery.config_from_object('celeryconfig')
 
+if True:
+    from sqlalchemy import create_engine
+    from sqlalchemy.pool import QueuePool
+    from sqlalchemy.orm import scoped_session
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('sqlite:///./moebius.sqlite', poolclass=QueuePool)
+    conn = engine.connect()
+    models.get_session = scoped_session(sessionmaker(bind=conn))
+
 @celery.task
 def get(location):
     r = requests.get(location)
 
     if r.status_code == 200:
         models.Pages.add(url=location, encoding=r.encoding, content=r.text)
+        u = models.URLs.parse(location)
+        u.mark(2)
 
 
 @celery.task
@@ -39,9 +51,7 @@ def parse(page_id):
 def fetch():
     u = models.URLs.head()
     print(u.unparse())
+    get.delay(u)
     u.mark(1)
     
-
-
-
 
